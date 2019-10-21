2a62d44bab5a1d8cadeccb0a6e26ffc1304ed007
rithaven <veneruth4@gmail.com>
2019-10-21 11:02:25 +0200

    Style login page

--- likeinst/urls.py
+++ likeinst/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     url('^$', views.welcome, name = 'welcome'),
     url(r'^$', views.profile, name = 'profile'),
     url(r'^$', views.timeline, name = 'timeline'), 
+    url(r'^image/(\d+)',views.single_image, name='single_image'),
+    url(r'^comment/(?P<id>\d+)', views.comment,name='comment'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^single_image/(\d+)',views.single_image, name= 'single_image'),
+    url(r'^send/',views.send, name='send'),
     url(r'^search/', views.search_results, name = 'search_results'),
     url(r'^upload/profile', views.upload_profile, name = 'upload_profile')
 
